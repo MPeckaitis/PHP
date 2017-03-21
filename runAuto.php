3b70b84dbcb002b1a14077fb6491c9673daeec41
Martynas Pečkaitis <peckaitis.martynas@gmail.com>
2017-03-21 14:19:03 +0200

    added use statemen and created new objects

--- runAuto.php
+++ runAuto.php
@@ -6,3 +6,11 @@
  * Time: 2:10 PM
  */
 require_once ("auto.php");
+
+use mano\pirmas\pirmasLygis;
+use mano\pirmas\antras\antrasLygis;
+use mano\pirmas\antras\trecias\treciasLygis;
+
+$nr1 = new pirmasLygis();
+$nr2 = new antrasLygis();
+$nr3 = new treciasLygis();
